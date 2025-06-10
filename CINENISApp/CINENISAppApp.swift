9e4aa9e3a7c7086abe757622e6e047f678e6579a
Ruth Manriquez <ruthmanriquez@192.168.1.13>
2025-06-10 10:47:00 -0600

    Primer commit del proyecto CINENISApp

---
+++ CINENISApp/CINENISAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CINENISAppApp.swift
+//  CINENISApp
+//
+//  Created by Ruth Manriquez on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CINENISAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CINENISApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CINENISApp
+//
+//  Created by Ruth Manriquez on 10/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
