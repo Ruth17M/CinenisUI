ff7bff94e30edce76dd0acb875c0427925aa34ab
CamLc05 <camilovicmx@gmail.com>
2025-06-10 13:39:25 -0600

    Initial Commit

---
+++ Cinenis3erParcial/Cinenis3erParcialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cinenis3erParcialApp.swift
+//  Cinenis3erParcial
+//
+//  Created by ISSC_411_2024 on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Cinenis3erParcialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
