ef329b0341b77ad8a90e35a668db63199ce16d54
CamLc05 <camilovicmx@gmail.com>
2025-06-13 19:15:55 -0600

    cambios peliculas

--- Cinenis3erParcial/VIEWS/MainView.swift
+++ Cinenis3erParcial/VIEWS/MainView.swift
@@ -31,6 +31,7 @@ struct MainView : View {
                         
                     }
                     .padding(.leading, 80)
+                    .padding(.vertical, 15)
                     
                     
                     HStack{
